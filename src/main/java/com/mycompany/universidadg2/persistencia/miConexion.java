2f3ae440d1e0261a09515655b6076b99729d3ba0
ezeSosa89 <113646638+ezeSosa89@users.noreply.github.com>
2022-10-18 11:45:31 -0300

    Revert "coso"
    This reverts commit 1be12673b55b5bc07d7ef958710634a1742ad13d.

--- src/main/java/com/mycompany/universidadg2/persistencia/miConexion.java
+++ src/main/java/com/mycompany/universidadg2/persistencia/miConexion.java
@@ -10,7 +10,6 @@ public class miConexion {
     private String url;
     private String usuario;
     private String password;
-<<<<<<< Updated upstream
     private Connection conexion;
 
     public miConexion(String url, String usuario, String password) {
@@ -24,22 +23,6 @@ public class miConexion {
             try{
                 Class.forName("org.mariadb.jdbc.Driver");
             }catch(ClassNotFoundException ex){
-=======
-    
-    private Connection conexion; // lo que importa
-    
-    public miConexion(String url, String usr, String pass)  {
-    this.url = url;
-    usuario= usr;
-    password = pass;
-    }
-    public Connection buscarConexion() {
-        if (conexion==null) { // si es la primera vez
-            try {
-                Class.forName("org.mariadb.jdbc.Driver"); //com.mysql.jdbc.Driver
-                    conexion = DriverManager.getConnection(url, usuario, password);
-            } catch (SQLException | ClassNotFoundException ex)  {
->>>>>>> Stashed changes
                 Logger.getLogger(miConexion.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
