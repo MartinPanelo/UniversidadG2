bdff747bfbc6a6b6cd84fdf5b67ae4ef82b2ecf9
ezeSosa89 <113646638+ezeSosa89@users.noreply.github.com>
2022-10-24 11:09:58 -0300

    InscripcionData

--- src/main/java/com/mycompany/universidadg2/UniversidadG2.java
+++ src/main/java/com/mycompany/universidadg2/UniversidadG2.java
@@ -6,9 +6,11 @@
 package com.mycompany.universidadg2;
 
 import com.mycompany.universidadg2.entidades.Alumno;
+import com.mycompany.universidadg2.entidades.Inscripcion;
 import com.mycompany.universidadg2.entidades.Materia;
 import com.mycompany.universidadg2.persistencia.AlumnoData;
 import com.mycompany.universidadg2.persistencia.Conexion;
+import com.mycompany.universidadg2.persistencia.InscripcionData;
 import com.mycompany.universidadg2.persistencia.MateriaData;
 import java.sql.Connection;
 import java.time.LocalDate;
@@ -22,48 +24,66 @@ public class UniversidadG2 {
     public static void main(String[] args) {
         
         Connection conexiondb = Conexion.getConexion();        
-        AlumnoData alu = new AlumnoData(conexiondb);
         
-       
-        // BUSCAR UN ALUMNO
-        System.out.println(alu.buscarAlumnoPorID(2));
-        
+          // CONEXION A ALUMNO
+          AlumnoData aData = new AlumnoData(conexiondb);
+          
+//        // BUSCAR UN ALUMNO
+//        System.out.println(alu.buscarAlumnoPorID(2));
+//        
+//        // GUARDAR UN ALUMNO
+//       
+//       Alumno aluGuardado = new Alumno(454545, "Mcwin", "Rayo",LocalDate.now() , true);
+//       alu.guardarAlumno(aluGuardado);
+//       
+//       // LISTAR ALUMNOS
+//       
+//        System.out.println(alu.listarAlumnos());
+//        
+//       // ACTUALIZAR UN ALUMNO // el id viene dentro del alumno
+//       Alumno aluActualizado = new Alumno(99999, "Zapallo", "Coreano",LocalDate.now() , true);
+//       alu.actualizarAlumno(aluActualizado);
+//       
+//       
+//       // BORRAR ALUMNO POR ID       
+//       alu.borrarAlumno(12);
 
-        // GUARDAR UN ALUMNO
-       
-       Alumno aluGuardado = new Alumno(454545, "Mcwin", "Rayo",LocalDate.now() , true);
-       alu.guardarAlumno(aluGuardado);
-       
-       // LISTAR ALUMNOS
-       
-        System.out.println(alu.listarAlumnos());
-        
-        
-       // ACTUALIZAR UN ALUMNO // el id viene dentro del alumno
-       Alumno aluActualizado = new Alumno(99999, "Zapallo", "Coreano",LocalDate.now() , true);
-       alu.actualizarAlumno(aluActualizado);
-       
-       
-       // BORRAR ALUMNO POR ID       
-       alu.borrarAlumno(12);
-       
-       //AGREGAR MATERIA
-       Materia matAgregar = new Materia("Matematica", 1, true);
-       MateriaData mData = new MateriaData(conexiondb);
-       mData.agregarMateria(matAgregar);
-       
-       // BUSCAR UNA MATERIA POR ID
-        System.out.println(mData.buscarMateriaPorID(4));
-        
-       // ACTUALIZAR UNA MATERIA
-       Materia mat2Agregar = new Materia(4, "Frances", 1, true);
-       mData.actualizarMateria(mat2Agregar);
-       
-       // LISTAR MATERIAS
-       System.out.println(mData.listarMaterias());
-       
-       // BORRAR MATERIAS
-       mData.borrarMateria(4);
+         // CONEXION A MATERIA
+         MateriaData mData = new MateriaData(conexiondb);
+//       
+//       //AGREGAR MATERIA
+//       Materia matAgregar = new Materia("Matematica", 1, true);
+//       mData.agregarMateria(matAgregar);
+//       
+//       // BUSCAR UNA MATERIA POR ID
+//        System.out.println(mData.buscarMateriaPorID(4));
+//        
+//       // ACTUALIZAR UNA MATERIA
+//       Materia mat2Agregar = new Materia(4, "Frances", 1, true);
+//       mData.actualizarMateria(mat2Agregar);
+//       
+//       // LISTAR MATERIAS
+//       System.out.println(mData.listarMaterias());
+//       
+//       // BORRAR MATERIAS
+//       mData.borrarMateria(4);
+
+         // CONEXION A INSCRIPCION
+         InscripcionData iData = new InscripcionData(conexiondb);
+
+         // GUARDAR INSCRIPCION
+//         Materia mateAgregar = mData.buscarMateriaPorID(3);
+//         Alumno alumGuardado = aData.buscarAlumnoPorID(2);
+//         
+//         Inscripcion in = new Inscripcion(alumGuardado, mateAgregar, 8);
+//         iData.guardarInscripcion(in);
+
+        // OBTENER INSCRIPCION
+//         System.out.println(iData.obtenerInscripcion(2, 2));
+
+        // ELIMINAR INSCRIPCION
+           iData.borrarInscripcion(3, 3);
+           
        
        
 //1 InscribirAlumnos - Alumno, Materia, Nota;
--- src/main/java/com/mycompany/universidadg2/persistencia/InscripcionData.java
+++ src/main/java/com/mycompany/universidadg2/persistencia/InscripcionData.java
@@ -5,10 +5,13 @@
 package com.mycompany.universidadg2.persistencia;
 
 import com.mycompany.universidadg2.entidades.Alumno;
+import com.mycompany.universidadg2.entidades.Inscripcion;
+import com.mycompany.universidadg2.entidades.Materia;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
@@ -19,10 +22,82 @@ import javax.swing.JOptionPane;
 public class InscripcionData {
     
     private Connection conexionData;
+    private AlumnoData a1 = new AlumnoData(conexionData);
+    private MateriaData m1 = new MateriaData(conexionData);
     
     public InscripcionData(Connection connection) {
         this.conexionData = connection;
     }
     
+    public void guardarInscripcion(Inscripcion i) {
+    String query = "INSERT INTO inscripcion(id_alumno, id_materia, nota) VALUES (?, ?, ?)";
+    try {
+    PreparedStatement ps = conexionData.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, i.getAlumno().getId_alumno());
+            ps.setInt(2, i.getMateria().getId_materia());
+            ps.setDouble(3, i.getNota());
+                if (ps.executeUpdate() > 0) {
+                    JOptionPane.showMessageDialog(null, "La inscripcion se realizo.");
+                } else  {
+                    JOptionPane.showMessageDialog(null, "La inscripcion no se realizo.");
+                }
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    i.setId_inscripcion(rs.getInt(1));
+                } else  {
+                    JOptionPane.showMessageDialog(null, "No se pudo obtener id.");
+                }
+            } catch(SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Se produjo un error, en agregar materia");
+            }
+    }
     
-}
+    public Inscripcion obtenerInscripcion(int id_alumno, int id_materia) {
+        Inscripcion i1 = null;
+        String sql = "SELECT * FROM inscripcion WHERE id_materia = ? AND id_alumno = ?";
+        try {
+            PreparedStatement ps = conexionData.prepareStatement(sql);
+            ps.setInt(1, id_materia);
+            ps.setInt(2, id_alumno);
+            ResultSet rs = ps.executeQuery();
+  
+            if (rs.next()) {
+                i1 = new Inscripcion();
+                i1.setAlumno(a1.buscarAlumnoPorID(rs.getInt("id_alumno")));
+                i1.setMateria(m1.buscarMateriaPorID(rs.getInt("id_materia")));
+                i1.setNota(rs.getDouble("nota"));
+                i1.setId_inscripcion(rs.getInt("id_inscripcion"));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Se produjo un error. obtener inscripcion.");
+        }
+//        
+        if (i1 == null) {
+            JOptionPane.showMessageDialog(null, "No hay una inscripcion con esos parametros.");
+        }
+        return i1;
+//   
+    }
+    
+     public void borrarInscripcion (int id_alumno, int id_materia){
+        String sql="DELETE FROM inscripcion WHERE id_alumno = ? AND id_materia = ?";
+        try {
+            PreparedStatement ps=conexionData.prepareStatement(sql);
+            ps.setInt(1, id_alumno);
+            ps.setInt(2, id_materia);
+            
+            if (ps.executeUpdate() <= 0) {
+                JOptionPane.showMessageDialog(null, "No se pudo eliminar");
+            } else  {   
+                JOptionPane.showMessageDialog(null, "Se elimino la inscripcion correctamente");
+            }
+            
+            ps.close();
+            
+    }   catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "InscripcionData Sentencia SQL erronea-borrarInscripcion");
+        }
+    }
+    
+    }
+    
